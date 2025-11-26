bf3d4c889105abdb09edc1cbaf8b69f643f9649a
JpCosta135 <130411184+JpCosta135@users.noreply.github.com>
2025-11-26 17:14:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -111,7 +111,8 @@ app.get('/', (req, res) => {
 //}); 
 
 app.listen(3000, () => {
-    console.log(`> Ready on http://0.0.0.0:3000`);
+    console.log(`> Ready on http://localhost:3000`);
 
 });
+
 
